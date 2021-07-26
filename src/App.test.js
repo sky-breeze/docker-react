efdd66d3e74c92815525a74af0d4e813b7a834ad
Pavan Pandey <pandeypavan03@gmail.com>
2021-07-26 09:31:06 +0530

    trevis file added

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-it('render without crashing',()=>{
-  const div = document.createElement('div')
-  ReactDOM.render(<App/>,div)
-  ReactDOM.unmountComponentAtNode(div)
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
+// it('render without crashing',()=>{
+//   const div = document.createElement('div')
+//   ReactDOM.render(<App/>,div)
+//   ReactDOM.unmountComponentAtNode(div)
+// });
